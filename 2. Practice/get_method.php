90a15d9db26d85f50e591c61b3a9fc087658ceca
pphatdev <leatsophat02@gmail.com>
2025-10-06 22:45:42 +0700

    Add get_method.php file to demonstrate handling form submission using GET method

---
+++ 2. Practice/get_method.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Method</title>
+</head>
+<body>
+    <h1>Get Method Example</h1>
+    <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div>
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" required>
+        </div>
+        <button type="submit" value="Submit">Submit</button>
+    </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $name = htmlspecialchars($_GET["name"] ?? 'N/A');
+            $age = htmlspecialchars($_GET["age"] ?? '00');
+
+            echo "<h2>Your Input:</h2>";
+            echo "Name: $name <br>";
+            echo "Age: $age <br>";
+        }
+    ?>
+</body>
+</html>
