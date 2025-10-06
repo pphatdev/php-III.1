02a3925972b2733a530fbc5f7aedbc4f645d9f24
pphatdev <leatsophat02@gmail.com>
2025-10-06 22:44:45 +0700

    Add echo.php file with basic arithmetic operations and output

---
+++ 2. Practice/echo.php
@@ -0,0 +1,18 @@
+<?php
+
+$string = "Hello World!, Echoing from echo.php";
+echo "{$string} <br/> <br/>";
+
+$a = 1;
+$b = 2;
+$c = $a + $b;
+echo "The sum of {$a} and {$b} is {$c} <br/> <br/>";
+
+$d = $a * $b;
+echo "The product of {$a} and {$b} is {$d} <br/> <br/>";
+
+$e = $b / $a;
+echo "The division of {$b} by {$a} is {$e} <br/> <br/>";
+
+$f = $b - $a;
+echo "The subtraction of {$a} from {$b} is {$f} <br/> <br/>";
