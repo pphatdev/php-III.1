171e597b09fe2e08e85f3741a1fc0532de0a0f79
pphatdev <leatsophat02@gmail.com>
2025-10-06 22:45:37 +0700

    Add sort.php file to demonstrate sorting arrays in ascending and descending order

---
+++ 2. Practice/sort.php
@@ -0,0 +1,21 @@
+<?php
+
+$array = [
+    "Apple",
+    "Orange",
+    "Banana",
+    "Grapes",
+    "Pineapple"
+];
+
+// sort in ascending order
+sort($array);
+print_r($array);
+
+echo "<br/> <br/>";
+
+// sort in descending order
+rsort($array);
+print_r($array);
+
+echo "<br/> <br/>";
