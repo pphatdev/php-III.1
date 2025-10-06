68944e50b4f8aac3d2a6c88f122e33097a6af074
pphatdev <leatsophat02@gmail.com>
2025-10-06 22:44:54 +0700

    Add print.php file with string output and basic arithmetic operations

---
+++ 2. Practice/print.php
@@ -0,0 +1,19 @@
+<?php
+
+$string = "Hello World!, Printing from print.php";
+print "{$string} <br/> <br/>";
+
+$a = 1;
+$b = 2;
+$c = $a + $b;
+print "The sum of {$a} and {$b} is {$c} <br/> <br/>";
+
+$d = $a * $b;
+print "The product of {$a} and {$b} is {$d} <br/> <br/>";
+
+$e = $b / $a;
+print "The division of {$b} by {$a} is {$e} <br/> <br/>";
+
+$f = $b - $a;
+print "The subtraction of {$a} from {$b} is {$f} <br/> <br/>";
+
